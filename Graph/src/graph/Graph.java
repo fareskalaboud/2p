5927f5d442b321b011ee84c1bd66e092121db926
SeanNaren <taz838@hotmail.co.uk>
2014-11-21 19:31:27 +0000

    Changed background colour and margins.

--- Graph/src/graph/Graph.java
+++ Graph/src/graph/Graph.java
@@ -16,6 +16,7 @@ import org.achartengine.renderer.XYSeriesRenderer;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.Paint.Align;
 
 /**
  * This class defines the graph that visualises the datasets recieved from the user input from the Central Bank. 
@@ -92,18 +93,23 @@ public class Graph {
 	{
 		//Sets the size of the title(I don't even think there is a title).
 		renderer.setAxisTitleTextSize(40);
+		
 		//Sets the size of the Chart title(I don't think there is a title).
 		renderer.setChartTitleTextSize(60);
 		//Sets the size of the labels on X and Y.
 		renderer.setLabelsTextSize(40);
 		//Sets the size of the keys for each graph. 
 		renderer.setLegendTextSize(16);
-		//This is used to set the default number of Labels on the Y axis. to increase labels, increase number. 
+		//This is used to set the default number of Labels on the X & Y axis. to increase labels, increase number. 
 		renderer.setYLabels(16);
+	
 		//Sets the size of the individual points.
 		renderer.setPointSize(7f);
 		//We increase the margin size on the left side of the screen to prevent clipping of the axis. 
-		renderer.setMargins(new int[] {0, 60, 0, 0});
+		renderer.setMargins(new int[] {0, 60, 60, 0});
+		
+		renderer.setYLabelsAlign(Align.RIGHT, 0);
+		renderer.setFitLegend(true);
 		//We iterate through the  number of sets, creating a specific renderer for each one, changing the colour every time. 
 		for(int i = 0;i<numberOfSets;i++)
 		{
@@ -118,10 +124,21 @@ public class Graph {
 		}
 		//The grid layout on the chart. 
 		renderer.setShowGrid(true);
+		renderer.setGridColor(Color.DKGRAY);
 		//Set the colours of the labels and the axis. 
-		renderer.setAxesColor(Color.DKGRAY);
-		renderer.setLabelsColor(Color.LTGRAY);
-
+		renderer.setAxesColor(Color.BLACK);
+		
+		renderer.setLabelsColor(Color.BLACK);
+		
+		renderer.setApplyBackgroundColor(true);
+		
+		renderer.setBackgroundColor(Color.WHITE);
+		
+		renderer.setMarginsColor(Color.WHITE);
+		
+		renderer.setXLabelsColor(Color.BLACK);
+		
+		renderer.setYLabelsColor(0, Color.BLACK);
 		//We create the intent and return it. 
 		Intent LineChart = ChartFactory.getTimeChartIntent(context, dataSet, renderer,"dd/MM/yyyy");
 		return LineChart;
