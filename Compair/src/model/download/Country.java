a298066a388653e1c4ac2da229ad5e6d0d4be7b9
rails-v <maria.vrabie@kcl.ac.uk>
2014-11-22 14:53:12 +0000

    Country class
    Country class with basic methods.

---
+++ Compair/src/model/download/Country.java
@@ -0,0 +1,40 @@
+package model.download;
+
+public class Country {
+
+	private String id;
+	private String capital;
+	private String name;
+	private String latitude;
+	private String longitude;
+
+	public Country() {
+
+		name = this.name;
+		id = this.id;
+		capital = this.capital;
+		longitude = this.longitude;
+		latitude = this.latitude;
+
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getCapital() {
+		return capital;
+	}
+
+	public String getLatitude() {
+		return latitude;
+	}
+
+	public String getLongitude() {
+		return longitude;
+	}
+}
