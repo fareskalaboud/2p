348c3329dad8a89e2810b5bcb4df8681ceae4055
Fares Alaboud <fares.alaboud@gmail.com>
2014-11-22 12:03:47 +0000

    Fixed background image issue, added Fonts classes and font files

--- Compair/src/seg2/compair/MainActivity.java
+++ Compair/src/seg2/compair/MainActivity.java
@@ -2,14 +2,15 @@ package seg2.compair;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Typeface;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.*;
 import model.Country;
 import model.download.JSONParser;
 
@@ -21,6 +22,8 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+        Fonts.makeFonts(this);
 
 		HashMap<String, Country> countries = new HashMap<String, Country>();
 		if (isInternetAvailable()) {
@@ -70,4 +73,41 @@ public class MainActivity extends Activity {
 		Toast.makeText(getApplicationContext(), "No Internet Connection...", Toast.LENGTH_SHORT).show();
 		return  false;
 	}
+
+    private class MyAdapter extends BaseAdapter {
+
+        private List<Object> objects; // obviously don't use object, use whatever you really want
+        private final Context context;
+
+        public MyAdapter(Context context, List<Object> objects) {
+            this.context = context;
+            this.objects = objects;
+        }
+
+        @Override
+        public int getCount() {
+            return objects.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return objects.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            Object obj = objects.get(position);
+
+            TextView tv = new TextView(context);
+            tv.setText(obj.toString()); // use whatever method you want for the label
+            tv.setTypeface(Fonts.LATO_BOLD);
+            return tv;
+        }
+    }
 }
