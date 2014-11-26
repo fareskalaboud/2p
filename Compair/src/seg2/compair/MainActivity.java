a92fcec50dfef18c9309789de079175c20e327b5
Fares Alaboud <fares.alaboud@gmail.com>
2014-11-26 13:54:57 +0000

    implemented a check for internet connection when the app launches

--- Compair/src/seg2/compair/MainActivity.java
+++ Compair/src/seg2/compair/MainActivity.java
@@ -1,9 +1,20 @@
 package seg2.compair;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.*;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import java.util.HashMap;
 
 public class MainActivity extends Activity {
@@ -15,11 +26,18 @@ public class MainActivity extends Activity {
 
 		Fonts.makeFonts(this);
 
+        if (!isInternetAvailable()) {
+            openAlert(new View(getApplicationContext()));
+        }
 	}
 
     public void goToCountrySelector(View view) {
-        Intent intent = new Intent(this, CountrySelectActivity.class);
-        startActivity(intent);
+        if (isInternetAvailable()) {
+            Intent intent = new Intent(this, CountrySelectActivity.class);
+            startActivity(intent);
+        } else {
+            Toast.makeText(this.getApplicationContext(), "You need a working internet connection!", Toast.LENGTH_LONG).show();
+        }
     }
 
     @Override
@@ -41,6 +59,16 @@ public class MainActivity extends Activity {
 		return super.onOptionsItemSelected(item);
 	}
 
+    private boolean isInternetAvailable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+            return true;
+        }
+        return  false;
+    }
+
 	// Get indicators that we are using from strings.xml
 	private HashMap<String, String> getIndicators () {
 		//Code to use in different classes
@@ -55,4 +83,32 @@ public class MainActivity extends Activity {
 
 		return indicators;
 	}
+
+    private void openAlert(View view) {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this, AlertDialog.THEME_HOLO_DARK);
+
+        alertDialogBuilder.setTitle("No Internet Connection");
+        alertDialogBuilder.setMessage("You need a working internet connection to use this app. Please check that you are connected.");
+        // set positive button: Yes message
+        alertDialogBuilder.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // go to a new activity of the app
+                startActivityForResult(new Intent(Settings.ACTION_WIFI_SETTINGS), 0);
+            }
+        });
+        // set negative button: No message
+        alertDialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // cancel the alert box and put a Toast to the user
+                dialog.cancel();
+                Toast.makeText(getApplicationContext(), "You chose a negative answer",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        // show alert
+        alertDialog.show();
+    }
+
 }
