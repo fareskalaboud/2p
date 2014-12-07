6528cd3b9efa3b9f4a15fa1e9a662440db91f29a
imex94 <imex94@gmail.com>
2014-12-07 16:55:35 +0000

    Alphabetical indicator ordering

--- Compair/src/seg2/compair/GraphActivity.java
+++ Compair/src/seg2/compair/GraphActivity.java
@@ -2,6 +2,8 @@ package seg2.compair;
 
 import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 
 import android.graphics.Color;
